d4d5bb994c564085adb83adc4bce2c4af564ad5b
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-12-10 08:02:55 +0530

    Created Basic project Structure.

--- lib/screens/input_form_screen.dart
+++ lib/screens/input_form_screen.dart
@@ -5,6 +5,30 @@ class InputFormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("First Name"),
+          Container(
+            width: double.infinity,
+            height: 20,
+            color: Colors.red,
+          ),
+          const Text("Last Name"),
+          Container(
+            width: double.infinity,
+            height: 20,
+            color: Colors.red,
+          ),
+          const Text("Age"),
+          Container(
+            width: double.infinity,
+            height: 20,
+            color: Colors.red,
+          )
+        ],
+      ),
+    );
   }
 }
