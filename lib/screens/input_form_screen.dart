448ffb981e73ec6d5d047341e31cacc0091694e4
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-12-10 10:09:36 +0530

    Added Value Notifiers.

--- lib/screens/input_form_screen.dart
+++ lib/screens/input_form_screen.dart
@@ -7,11 +7,16 @@ class InputFormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    final firstNameNotifier = ValueNotifier<String>("");
+    final lastNameNotifier = ValueNotifier<String>("");
+    final ageNotifier = ValueNotifier<double>(0);
+
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             "Input Data Form",
             style: TextStyle(
               fontSize: HEADER_TEXT_SIZE,
@@ -20,15 +25,31 @@ class InputFormScreen extends StatelessWidget {
           ),
           FormInputWidget(
             name: "First Name",
-            child: TextField(),
+            child: TextField(
+              onChanged: (String value) {
+                firstNameNotifier.value = value;
+              },
+            ),
           ),
           FormInputWidget(
             name: "Last Name",
-            child: TextField(),
+            child: TextField(
+              onChanged: (String value) {
+                lastNameNotifier.value = value;
+              },
+            ),
           ),
           FormInputWidget(
             name: "Age",
-            child: TextField(),
+            child: TextField(
+              onChanged: (String value) {
+                try {
+                  ageNotifier.value = double.parse(value);
+                } catch(ex) {
+                  print("$ex");
+                }
+              },
+            ),
           ),
         ],
       ),
